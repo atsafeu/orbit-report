c57d11485c3b3880f5251d722ee68d26eb5c53ec
Armand Tsafack <armandtsafack@armandtsafack.home>
2020-09-20 20:10:55 -0500

    final6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
-  displayList: Satellite[]
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
     this.displayList = [];
@@ -21,6 +21,7 @@ export class AppComponent {
           for(let i=0; i<satellitesUrl.length;i++) {
             let satellite=new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
               this.sourceList.push(satellite);
+             
             }
         this.displayList=this.sourceList.slice(0);
        }.bind(this));
@@ -28,6 +29,7 @@ export class AppComponent {
  
     
  }
+ ngOnInit(): void { };
  search(searchTerm: string): void {
   let matchingSatellites: Satellite[] = [];
   searchTerm = searchTerm.toLowerCase();
